b4c1f3a199a853150898e7f9bc9b844ab5c4f8d4
YuriFont <yurifontenelesilva180303@gmail.com>
2023-11-14 02:53:12 -0300

    F

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 21:41:31 by yufonten          #+#    #+#             */
-/*   Updated: 2023/11/14 02:31:46 by yufonten         ###   ########.fr       */
+/*   Updated: 2023/11/14 02:53:01 by yufonten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 10
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
