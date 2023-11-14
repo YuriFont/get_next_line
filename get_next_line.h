961213830263ef5fd7b3c613ca5ff8f5600ce957
YuriFont <yurifontenelesilva180303@gmail.com>
2023-11-14 02:46:52 -0300

    F

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 21:41:31 by yufonten          #+#    #+#             */
-/*   Updated: 2023/11/13 23:45:56 by yufonten         ###   ########.fr       */
+/*   Updated: 2023/11/14 02:31:46 by yufonten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+#endif
 
 # include <unistd.h>
 # include <stdlib.h>
@@ -27,6 +31,7 @@ t_list	*find_last_node(t_list *list);
 int		len_to_newline(t_list *list);
 void	copy_str_list(t_list *list, char *str);
 void	clear_list(t_list **list, t_list *clean_node, char *buf);
+
 char	*get_next_line(int fd);
 
 #endif
